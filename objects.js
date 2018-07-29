7f5acb3f13e92402a9f97c1b9300513d919da877
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-07-29 18:04:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object ({ artistname: 'song title'})
+var playlist = {artistName: 'songtitle'}
 function updatePlaylist (playlist,artistName,songTitle){
   playlist[artistName] = 'song'
   return playlist
